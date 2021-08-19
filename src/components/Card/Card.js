50a1f952eea5a84cb8088e8d2dd6b5655e15b9bd
xingcdev <60323726+xingcdev@users.noreply.github.com>
2021-08-19 18:53:11 +0200

    add glass card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Card = function ({ children }) {
-	return <div class="bg-gray rounded-2xl mx-3">{children}</div>;
+	return (
+		<div class="bg-white bg-opacity-25 backdrop-filter backdrop-blur-2xl rounded-2xl mx-3 pt-3 pl-3 pb-3">
+			{children}
+		</div>
+	);
 };
 
 export default Card;
