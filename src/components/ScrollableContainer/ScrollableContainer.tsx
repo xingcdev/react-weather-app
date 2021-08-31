8358b5414dfc1210d558816f428b10ee20cef75b
xingcdev <60323726+xingcdev@users.noreply.github.com>
2021-08-31 23:19:12 +0200

    Make scrollable container using Swiper

--- src/components/ScrollableContainer/ScrollableContainer.tsx
+++ src/components/ScrollableContainer/ScrollableContainer.tsx
@@ -1,3 +1,15 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 export default function ScrollableContainer(props) {
-	return <div className="overflow-x-scroll no-scrollbar">{props.children}</div>;
+	return (
+		<Swiper
+			slidesPerView={'auto'}
+			freeMode={true}
+			// Prevent the whole page to be swiped
+			touchMoveStopPropagation={true}
+		>
+			{/* The width is the chart's */}
+			<SwiperSlide style={{ width: '1200px' }}>{props.children}</SwiperSlide>
+		</Swiper>
+	);
 }
