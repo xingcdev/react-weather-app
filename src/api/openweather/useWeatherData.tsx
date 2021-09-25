03e73d30c4a4758ef145caed7c5595060d430e49
xingcdev <60323726+xingcdev@users.noreply.github.com>
2021-09-25 13:09:31 +0200

    allow useWeatherData hook to fetch daily data

--- src/api/openweather/useWeatherData.tsx
+++ src/api/openweather/useWeatherData.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
-function useWeatherData() {
-	const [weatherData, setWeatherData] = useState(null);
+function useWeatherData(dataType?: string) {
+	let apiUrl = `${process.env.REACT_APP_WEATHER_API_URL}onecall?lat=48.856614&lon=2.3522219&units=metric&&exclude=minutely,daily,alerts&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+	if (dataType === 'daily') {
+		apiUrl = `${process.env.REACT_APP_WEATHER_API_URL}onecall?lat=48.856614&lon=2.3522219&units=metric&&exclude=current,minutely,hourly,alerts&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+	}
 
+	const [weatherData, setWeatherData] = useState(null);
 	useEffect(() => {
 		const fetchData = async function () {
-			await fetch(
-				`${process.env.REACT_APP_WEATHER_API_URL}onecall?lat=48.856614&lon=2.3522219&units=metric&&exclude=minutely,daily,alerts&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-			)
+			await fetch(apiUrl)
 				.then((res) => res.json())
 				.then((result) => {
 					setWeatherData(result);
-					console.log(result);
 				});
 		};
 		fetchData();
--- src/views/Details/DetailsPage/DetailsPage.tsx
+++ src/views/Details/DetailsPage/DetailsPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import DailyForecast from '../DailyForecast/DailyForecast';
+import useWeatherData from '../../../api/openweather/useWeatherData';
+
+interface WeatherData {
+	daily: { [key: string]: any }[];
+}
 
 export default function DetailsPage() {
+	const weatherData: WeatherData = useWeatherData('daily');
+	console.log(weatherData);
+
+	if (!weatherData) return <p>Loading...</p>;
+
 	return (
 		<div className="p-4 lg:rounded-lg lg:shadow-md lg:col-start-8 lg:col-end-11">
 			<DailyForecast />
